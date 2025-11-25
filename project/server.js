16ccfca904510b39a0d64180a99f2398de3215ba
Hooria_Fatima <fatimahooria717@gmail.com>
2025-11-25 12:47:28 +0500

    first commit

--- project/server.js
+++ project/server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
+// Public images serving
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 app.use(cors());
 app.use(bodyParser.json());
-
-// Public folder for images
-app.use('/images', express.static('public/images'));
 
 // Routes
 const hallsRoutes = require("./routes/halls");
 app.use("/api/halls", hallsRoutes);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
